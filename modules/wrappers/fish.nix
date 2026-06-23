c6dc077f92a18b33bf4ae183cb9912c6e7320c04
Max Allfrey <max.allfrey@gmail.com>
2026-06-23 20:04:10 +0100

    Replace hydro with pure

--- modules/wrappers/fish.nix
+++ modules/wrappers/fish.nix
@@ -11,11 +11,10 @@
     imports = [wlib.wrapperModules.fish];
     runtimePkgs = [pkgs.zoxide];
 
-    plugins = [pkgs.fishPlugins.hydro];
+    plugins = [pkgs.fishPlugins.pure];
 
     shellAliases = rec {
       ls = "${lib.getExe pkgs.eza} --icons --follow-symlinks";
-      lst = "${ls} --tree --icons";
       cat = "${lib.getExe pkgs.bat} --theme=base16";
       v = lib.getExe self'.packages.neovim;
     };
@@ -55,45 +54,4 @@
         end
       '';
   });
-  #  perSystem = {
-  #    pkgs,
-  #    lib,
-  #    ...
-  #  }: {
-  #    packages.fish = inputs.wrapper-modules.wrappers.fish.wrap {
-  #      inherit pkgs;
-  #      runtimePkgs = [pkgs.zoxide];
-  #
-  #      shellAliases = rec {
-  #        ls = "${lib.getExe pkgs.eza} --icons --follow-symlinks";
-  #        lst = "${ls} --tree --icons";
-  #        cat = "${lib.getExe pkgs.bat} --theme=base16";
-  #        v = "nvim";
-  #      };
-  #
-  #      abbreviations = {
-  #        d = "cd ~/Downloads";
-  #        infra = "cd ~/Projects/infra";
-  #        lsa = "ls -la";
-  #	lst = "ls --tree";
-  #      };
-  #
-  #      configFile.content =
-  #        # fish
-  #        ''
-  #          function fish_prompt
-  #              string join "" -- (set_color red) "[" (set_color yellow) $USER (set_color green) "@" (set_color blue) $hostname (set_color magenta) " " $(prompt_pwd) (set_color red) ']' (set_color normal) "\$ "
-  #          end
-  #
-  #          set fish_greeting
-  #          fish_vi_key_bindings
-  #
-  #          ${lib.getExe pkgs.zoxide} init fish | source
-  #
-  #          if type -q direnv
-  #            direnv hook fish | source
-  #          end
-  #        '';
-  #    };
-  #  };
 }
