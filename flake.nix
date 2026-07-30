0ee47d0c289eb4d4878dfae0461614db472f3029
Max Allfrey <max.allfrey@gmail.com>
2026-07-30 20:51:09 +0100

    Add Noctalia v5

--- flake.nix
+++ flake.nix
@@ -21,6 +21,7 @@
     };
     nixos-hardware.url = "github:nixos/nixos-hardware";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    noctalia.url = "github:noctalia-dev/noctalia/cachix";
     noctalia-plugins = {
       url = "github:noctalia-dev/noctalia-plugins";
       flake = false;
