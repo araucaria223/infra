572e02c6ec31eb2d573955da531877d766a05850
Max Allfrey <max.allfrey@gmail.com>
2026-08-04 22:57:37 +0100

    Chore: formatting

--- modules/wrappers/noctalia-v5/noctalia.nix
+++ modules/wrappers/noctalia-v5/noctalia.nix
@@ -1,10 +1,14 @@
-{self, moduleWithSystem, ...}: {
+{
+  self,
+  moduleWithSystem,
+  ...
+}: {
   flake-file.inputs.noctalia.url = "github:noctalia-dev/noctalia/cachix";
 
   flake.wrappers.noctalia-v5 = moduleWithSystem ({inputs', ...}: {...}: {
     imports = [self.wrapperModules.noctalia-wrapper];
     package = inputs'.noctalia.packages.default;
 
-    settings = builtins.fromTOML (builtins.readFile ./config.toml);
-});
+    settings = fromTOML (builtins.readFile ./config.toml);
+  });
 }
