e0a56c9b7c5bcaaa52d4f44b572fc40c7d0c8cd3
Max Allfrey <max.allfrey@gmail.com>
2026-08-06 00:24:55 +0100

    Flake: remove Noctalia input

--- flake.nix
+++ flake.nix
@@ -24,7 +24,6 @@
     };
     nixos-hardware.url = "github:nixos/nixos-hardware";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    noctalia.url = "github:noctalia-dev/noctalia/cachix";
     noctalia-greeter.url = "github:noctalia-dev/noctalia-greeter";
     preservation.url = "github:nix-community/preservation";
     treefmt-nix.url = "github:numtide/treefmt-nix";
