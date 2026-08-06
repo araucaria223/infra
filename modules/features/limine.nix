b463d71d29e242c9eb922502be75ae64eea86367
Max Allfrey <max.allfrey@gmail.com>
2026-08-06 20:00:00 +0100

    Limine: enable secure boot

--- modules/features/limine.nix
+++ modules/features/limine.nix
@@ -1,7 +1,16 @@
-{...}: {
-  flake.modules.nixos.limine = {...}: {
+{self, ...}: {
+  flake.modules.nixos.limine = {pkgs, ...}: {
     boot.loader.limine = {
       enable = true;
     };
+
+    # Needed to set up secure boot
+    environment.systemPackages = [pkgs.sbctl];
+  };
+
+  flake.modules.nixos.preservation = {...}: {
+    preservation.preserveAt."/persistent".directories = [
+      "/var/lib/sbctl"
+    ];
   };
 }
