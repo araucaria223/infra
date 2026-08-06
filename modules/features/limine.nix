b92812dbceb215949440f5d9eafe2b02c91f0501
Max Allfrey <max.allfrey@gmail.com>
2026-08-06 20:01:05 +0100

    Limine: add theme

--- modules/features/limine.nix
+++ modules/features/limine.nix
@@ -2,6 +2,19 @@
   flake.modules.nixos.limine = {pkgs, ...}: {
     boot.loader.limine = {
       enable = true;
+      secureBoot.enable = true;
+
+      style = with self.theme.paletteNoHash; {
+        graphicalTerminal = {
+          palette = "${base05};${base08};${base0B};${base0A};${base0D};${base0E};${base0C};${base00}";
+          brightPalette = "${base00};${base08};${base0B};${base0A};${base0D};${base0E};${base0C};${base05}";
+          background = base00;
+          foreground = base05;
+          brightBackground = base05;
+          brightForeground = base0A;
+        };
+        backdrop = base00;
+      };
     };
 
     # Needed to set up secure boot
