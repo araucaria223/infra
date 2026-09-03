3e753988725e30af935a357a4924046ae0e54b26
Max Allfrey <max.allfrey@gmail.com>
2026-09-03 02:11:11 +0100

    Remove obsolete option definition

--- modules/hosts/default.nix
+++ modules/hosts/default.nix
@@ -1,13 +1,6 @@
-{lib, ...}: {
+{
   flake-file.inputs.nixos-hardware = {
     url = "github:nixos/nixos-hardware";
     inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  flake.modules = {
-    options.disko = lib.mkOption {
-      type = lib.types.attrsOf lib.types.attrsOf lib.types.unspecified;
-      default = {};
-    };
   };
 }
