14a3c5da660445dba8307fe2ccc29afdb49abc3f
Max Allfrey <max.allfrey@gmail.com>
2026-09-03 18:05:44 +0100

    Steam: use FSR upscaling

--- modules/features/steam.nix
+++ modules/features/steam.nix
@@ -30,14 +30,35 @@
 
         steam = {
           enable = true;
-          gamescopeSession.enable = true;
+          gamescopeSession = {
+            enable = true;
+            args = [
+              "-W"
+              "2256"
+              "-H"
+              "1504"
+              "-f"
+              "-e"
+              "--xwayland-count"
+              "2"
+              "--hdr-enabled"
+              "--hdr-itm-enabled"
+            ];
+            steamArgs = [
+              "-pipewire-dmabuf"
+              "-gamepadui"
+              "-steamdeck"
+              "-steamos3"
+              "-mangoapp"
+            ];
+          };
         };
       };
 
       services.greetd = {
         enable = true;
         settings.default_session = {
-          command = "${lib.getExe pkgs.gamescope} -W 1920 -H 1080 -f -e --xwayland-count 2 --hdr-enabled --hdr-itm-enabled -- steam -pipewire-dmabuf -gamepadui -steamdeck -steamos3 > /dev/null 2>&1";
+          command = "${lib.getExe pkgs.gamescope} -W 1920 -H 1080 -w 1280 -h 720 -f -e --xwayland-count 2 --hdr-enabled --hdr-itm-enabled -F fsr -- steam -pipewire-dmabuf -gamepadui -steamdeck -steamos3 > /dev/null 2>&1";
           user = config.users.users.araucaria.name;
         };
       };
