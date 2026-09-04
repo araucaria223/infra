3602d297e78820f33df9cca3487ef0ce23908b36
Max Allfrey <max.allfrey@gmail.com>
2026-09-04 15:39:39 +0100

    Iso image: disable nixos user

--- modules/hosts/iso-image/default.nix
+++ modules/hosts/iso-image/default.nix
@@ -22,6 +22,7 @@
         ...
       }: {
         networking.hostName = "live";
+        users.users.nixos.enable = false;
         time.timeZone = "Europe/London";
         i18n.defaultLocale = "en_GB.UTF-8";
 
