a21f16259b32c2f6403029aeb56fe56b92d70764
Max Allfrey <max.allfrey@gmail.com>
2026-09-12 00:05:27 +0100

    Switch to correct cachyos kernel

--- modules/features/kernel.nix
+++ modules/features/kernel.nix
@@ -8,6 +8,6 @@
     };
 
     boot.kernelPackages =
-      inputs'.nix-cachyos-kernel.legacyPackages.linuxPackages-cachyos-bore-lto-x86_64-v4;
+      inputs'.nix-cachyos-kernel.legacyPackages.linuxPackages-cachyos-bore-lto-x86_64-v3;
   });
 }
